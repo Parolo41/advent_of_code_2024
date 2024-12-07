aba0096e5bc037f84f0b0409b77103a5f4d1c9a7
Parolo41 <ciril.neukom@bluewin.ch>
2024-12-07 13:45:58 +0100

    trying out borrowed efficiency change

--- advent_of_code_2024/src/day7b.rs
+++ advent_of_code_2024/src/day7b.rs
@@ -6,15 +6,13 @@ pub fn run() {
     'line_read: for line in fs::read_to_string("inputs/day7.txt").unwrap().lines() {
         let split_vals = line.split(":").collect::<Vec<&str>>();
 
-        println!("{line:?}");
-
         let calib_result = split_vals[0].parse::<u64>().expect("Operation result should be an integer");
         
         let calib_vals = split_vals[1].trim().split(" ").map(|v| v.parse().expect("Operation values should be integers")).collect::<Vec<u64>>();
 
         let possible_swaps = 3_usize.pow((calib_vals.len() - 1) as u32);
 
-        for i in 0..possible_swaps {
+        'swap_attempt: for i in 0..possible_swaps {
             let mut running_total = calib_vals[0] as u64;
             let mut swap_tracker = i;
 
@@ -24,16 +22,21 @@ pub fn run() {
                 match op_index {
                     0 => running_total += val,
                     1 => running_total *= val,
-                    2 => running_total = format!("{running_total}{val}").parse().expect("Concat should produce an integer"),
+                    2 => running_total = {
+                        let log = val.ilog10();
+                        let digit_mul = 10u64.pow(log + 1);
+                        running_total * digit_mul + val
+                    },
                     _ => panic!("Invalid op index: {op_index}"),
                 }
+
+                if running_total > calib_result { continue 'swap_attempt; }
 
                 swap_tracker /= 3;
             }
 
             if running_total == calib_result {
                 valid_calib_sum += calib_result;
-                println!("Valid");
                 continue 'line_read;
             }
         }
